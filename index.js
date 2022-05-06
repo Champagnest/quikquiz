5128230ccbea61538dd52f58f446032a8e8bac24
Champagnest <mchampagned@protonmail.com>
2022-05-06 12:17:12 -0700

    mostly done just need highscores saved

--- index.js
+++ index.js
@@ -14,18 +14,22 @@ start_btn.onclick = ()=>{
 }
 // if exitQuiz button clicked
 exit_btn.onclick = ()=>{
+        
+    
+
     info_box.classList.remove("activeInfo"); //hide info box
 }
 // if continueQuiz button clicked
 continue_btn.onclick = ()=>{
+    console.log("firstquestion")
     info_box.classList.remove("activeInfo"); //hide info box
     quiz_box.classList.add("activeQuiz"); //show quiz box
     showQuetions(0); //calling showQestions function
     queCounter(1); //passing 1 parameter to queCounter
-    startTimer(15); //calling startTimer function
-    startTimerLine(0); //calling startTimerLine function
+    startTimer(60); //calling startTimer function
+    // startTimerLine(0); //calling startTimerLine function
 }
-let timeValue =  15;
+let timeValue =  60;
 let que_count = 0;
 let que_numb = 1;
 let userScore = 0;
@@ -38,7 +42,7 @@ const quit_quiz = result_box.querySelector(".buttons .quit");
 restart_quiz.onclick = ()=>{
     quiz_box.classList.add("activeQuiz"); //show quiz box
     result_box.classList.remove("activeResult"); //hide result box
-    timeValue = 15; 
+    timeValue = 60; 
     que_count = 0;
     que_numb = 1;
     userScore = 0;
@@ -46,9 +50,9 @@ restart_quiz.onclick = ()=>{
     showQuetions(que_count); //calling showQestions function
     queCounter(que_numb); //passing que_numb value to queCounter
     clearInterval(counter); //clear counter
-    clearInterval(counterLine); //clear counterLine
+    // clearInterval(counterLine); //clear counterLine
     startTimer(timeValue); //calling startTimer function
-    startTimerLine(widthValue); //calling startTimerLine function
+    // startTimerLine(widthValue); //calling startTimerLine function
     timeText.textContent = "Time Left"; //change the text of timeText to Time Left
     next_btn.classList.remove("show"); //hide the next button
 }
@@ -65,15 +69,15 @@ next_btn.onclick = ()=>{
         que_numb++; //increment the que_numb value
         showQuetions(que_count); //calling showQestions function
         queCounter(que_numb); //passing que_numb value to queCounter
-        clearInterval(counter); //clear counter
-        clearInterval(counterLine); //clear counterLine
+        // clearInterval(counter); //clear counter
+        // clearInterval(counterLine); //clear counterLine
         startTimer(timeValue); //calling startTimer function
-        startTimerLine(widthValue); //calling startTimerLine function
+        // startTimerLine(widthValue); //calling startTimerLine function
         timeText.textContent = "Time Left"; //change the timeText to Time Left
         next_btn.classList.remove("show"); //hide the next button
     }else{
         clearInterval(counter); //clear counter
-        clearInterval(counterLine); //clear counterLine
+        // clearInterval(counterLine); //clear counterLine
         showResult(); //calling showResult function
     }
 }
@@ -101,7 +105,7 @@ let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 //if user clicked on option
 function optionSelected(answer){
     clearInterval(counter); //clear counter
-    clearInterval(counterLine); //clear counterLine
+    // clearInterval(counterLine); //clear counterLine
     let userAns = answer.textContent; //getting user selected option
     let correcAns = questions[que_count].answer; //getting correct answer from array
     const allOptions = option_list.children.length; //getting all option items
@@ -113,6 +117,7 @@ function optionSelected(answer){
         console.log("Correct Answer");
         console.log("Your correct answers = " + userScore);
     }else{
+        timeValue -= 5
         answer.classList.add("incorrect"); //adding red color to correct selected option
         answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
         console.log("Wrong Answer");
@@ -149,21 +154,23 @@ function showResult(){
     }
 }
 
-function startTimer(time){
+function startTimer(){
     counter = setInterval(timer, 1000);
+    console.log(timer)
     function timer(){
-        timeCount.textContent = time; //changing the value of timeCount with time value
-        time--; //decrement the time value
-        if(time < 9){ //if timer is less than 9
+        timeValue--; 
+        timeCount.textContent = timeValue;
+        if(timeValue < 9){ //if timer is less than 9
             let addZero = timeCount.textContent; 
             timeCount.textContent = "0" + addZero; //add a 0 before time value
         }
-        if(time < 0){ //if timer is less than 0
+        if(timeValue < 0){ //if timer is less than 0
             clearInterval(counter); //clear counter
             timeText.textContent = "Time Off"; //change the time text to time off
             const allOptions = option_list.children.length; //getting all option items
             let correcAns = questions[que_count].answer; //getting correct answer from array
             for(i=0; i < allOptions; i++){
+                console.log(option_list)
                 if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer
                     option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
                     option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
@@ -177,16 +184,7 @@ function startTimer(time){
         }
     }
 }
-function startTimerLine(time){
-    counterLine = setInterval(timer, 29);
-    function timer(){
-        time += 1; //upgrading time value with 1
-        time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-        if(time > 549){ //if time value is greater than 549
-            clearInterval(counterLine); //clear counterLine
-        }
-    }
-}
+
 function queCounter(index){
     //creating a new span tag and passing the question number and total question
     let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ questions.length +'</p> Questions</span>';
